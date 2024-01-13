08cd9025c0cf637190a2b5215d240644143dbba1
Keith <kayceecode@gmail.com>
2024-01-13 16:24:18 -0500

    starting auth endpoints

---
+++ Endpoints/AuthEndpoints.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace foodies_api;
+
+public static class AuthEndpoints
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly IConfiguration _configuration;
+
+    public AuthEndpoints(
+        UserManager<IdentityUser> userManager,
+        RoleManager<IdentityRole> roleManager,
+        IConfiguration configuration
+    ) 
+    {
+
+    }
+    public async static Task<IResult> Login(LoginModel model) 
+    {
+        var user = await _userManager.FindByNameAsync(model.Username);
+
+        if(userExists != null) 
+        {
+            return StatusCode(
+                StatusCodes.Status500InternalServerError, 
+                new Response { Status = "Error",
+                MessageProcessingHandler = "User already exists!"} );
+        }
+    }
+}
